9da778e7e3eec0606ded6c79f060acf0c26367a1
asifrazaansari <asifraza0231@gmail.com>
2022-10-24 08:34:20 +0530

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const router = require('./routes/route')
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
+const port = process.env.PORT
+const url = process.env.MONGODB_URL
 
-mongoose.connect("mongodb+srv://subhadip123:zLd05Hb78G3vrkvS@cluster0.c35uxw1.mongodb.net/group24Database?retryWrites=true&w=majority", 
-{useNewUrlParser :true})
-.then(() => console.log("MongoDb is connected"))
-.catch(err => console.log(err))
+mongoose.connect(url,
+    { useNewUrlParser: true })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
 
 app.use('/', router)
 
-app.use(function (req, res) {
-    return res.status(400).send({ status: false, message: "Path not found, please provide correct path" })
-})
 
-
-app.listen(process.env.PORT || 3000, function (req, res) {
-    console.log("Express app running on PORT " + (process.env.PORT || 3000))
+app.listen(port, function (req, res) {
+    console.log("Express app running on PORT " + (port))
 })
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -5,5 +5,8 @@ const {shortenUrl, originUrl} = require('../controllers/urlController')
 router.post("/url/shorten", shortenUrl)
 router.get("/:urlCode", originUrl)
 
+router.all('/*', async function (req, res) {
+    return res.status(400).send({ status: false, message: "Page not found" })
+})
 
 module.exports = router
